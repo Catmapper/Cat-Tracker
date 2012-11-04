8510bc3205b52d334e878659cb0f2d13d1648937
Joey Kudish <joachim.kudish@gmail.com>
2012-11-03 20:35:53 -0700

    new utility class which will contain helper functions for the Cat Tracker
    For now it includes functions to validate latitude and longitudes

---
+++ content/plugins/cat-tracker/classes/cat-tracker-utils.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package Cat_Tracker
+ * @author Joachim Kudish
+ * @version 1.0
+ *
+ * This class contains various utility functions
+ * that are helful to the Cat Tracker plugin
+ * This is a static class
+*/
+
+class Cat_Tracker_Utils {
+
+	public static function validate_latitude( $maybe_a_latitude ) {
+		return _validate_latitude_or_latitude_helper( $maybe_a_latitude, 90 );
+	}
+
+	public static function validate_longitude( $maybe_a_longitude ) {
+		return _validate_latitude_or_latitude_helper( $maybe_a_longitude, 180 );
+	}
+
+	private static function _validate_latitude_or_latitude_helper( $maybe_a_latitude_or_longitude, $range ) {
+		$range = (int) $range;
+		return ( ! empty( $maybe_a_longitude ) && is_numeric( $maybe_a_longitude ) && -$range <= $maybe_a_longitude && $range >= $maybe_a_longitude );
+	}
+
+}
