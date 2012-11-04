7db0a1d734a8ca871b42e611058262871d109611
Joey Kudish <joachim.kudish@gmail.com>
2012-11-03 20:35:16 -0700

    cat mapper only: filter the map source to display with the custom cloudmate styles. fixes #7

--- content/mu-plugins/cat-tracker.php
+++ content/mu-plugins/cat-tracker.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
-Plugin Name: Cat Tracker Modifier
+Plugin Name: Cat Tracker Modifier for catmapper.ca
 Plugin URI: https://github.com/jkudish/Cat-Tracker
-Description: Additional WordPress modifiers for the Cat Tracking Software
+Description: Additional modifiers for the Cat Tracking Software
 Version: 1.0
 Author: Joachim Kudish
 Author URI: http://jkudish.com/
@@ -38,8 +38,34 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * @since 1.0
  * @return void
  */
-add_action( 'admin_menu', 'cat_tracker_remove_admin_menus' );
-function cat_tracker_remove_admin_menus() {
+add_action( 'admin_menu', 'cat_mapper_remove_admin_menus' );
+function cat_mapper_remove_admin_menus() {
 	remove_menu_page( 'edit.php' ); // Posts
 	remove_menu_page( 'edit-comments.php' ); // Comments
+}
+
+/**
+ * filter the map source to display with the custom styles
+ *
+ * @since 1.0
+ * @return void
+ */
+add_filter( 'cat_tracker_map_source', 'cat_mapper_map_source' );
+function cat_mapper_map_source() {
+	if ( Cat_Tracker::is_submission_mode() || is_admin() )
+		return 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/75872/256/{z}/{x}/{y}.png';
+
+	return 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/75869/256/{z}/{x}/{y}.png';
+}
+
+/**
+ * filter the map attribution to display a notice about Cloudmade
+ *
+ * @since 1.0
+ * @return void
+ */
+add_filter( 'cat_tracker_map_attribution', 'cat_mapper_map_attribution' );
+function cat_mapper_map_attribution( $map_attribution ) {
+	$map_attribution .= ' &mdash; Map Styles © Cloudmade';
+	return $map_attribution;
 }
