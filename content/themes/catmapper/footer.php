c97a4b6f7abac30858119f81d75480122e526780
Joey Kudish <joachim.kudish@gmail.com>
2012-11-12 23:40:02 -0800

    minor theme cleanup, ref #13

--- content/themes/catmapper/footer.php
+++ content/themes/catmapper/footer.php
@@ -10,11 +10,6 @@
  */
 ?>
 	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
