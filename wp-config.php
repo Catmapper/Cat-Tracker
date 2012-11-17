1997885dd09fbdb9832f6fa4931b993c02ae0c6b
catmapper <catmapper@ps134988.dreamhostps.com>
2012-11-17 15:26:42 -0800

    proper wp-config.php rules for multisite ref #29

--- wp-config.php
+++ wp-config.php
@@ -28,13 +28,12 @@ define( 'WPLANG', '' );
 // ===================
 // Multisite
 // ===================
-define( 'WP_ALLOW_MULTISITE', true );
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', true );
-define( 'DOMAIN_CURRENT_SITE', 'catmapper.dev' );
-define( 'PATH_CURRENT_SITE', '/wp/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 
 // ===================
 // Bootstrap WordPress
