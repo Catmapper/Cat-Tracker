082d22db25105dee151a0afb23747613247991c7
Joey Kudish <joachim.kudish@gmail.com>
2012-11-16 22:57:57 -0800

    add wp-config.php file which adds multisite support, ref #29

---
+++ wp-config.php
@@ -0,0 +1,44 @@
+<?php
+// ===================================================
+// Load database info and environment specific development parameters
+// ===================================================
+if ( ! file_exists( dirname( __FILE__ ) . '/local-config.php' ) )
+	return;
+
+include( dirname( __FILE__ ) . '/local-config.php' );
+
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
+
+// ===================
+// Multisite
+// ===================
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', 'catmapper.dev' );
+define( 'PATH_CURRENT_SITE', '/wp/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
+// ===================
+// Bootstrap WordPress
+// ===================
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+require_once( ABSPATH . 'wp-settings.php' );
