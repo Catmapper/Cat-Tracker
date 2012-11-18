5c288370cb017d8a563b5e10198bd076746e60f3
Joey Kudish <joachim.kudish@gmail.com>
2012-11-17 19:19:24 -0800

    remove media from admin menu, ref #29

--- content/mu-plugins/catmapper.php
+++ content/mu-plugins/catmapper.php
@@ -44,6 +44,7 @@ function cat_mapper_remove_admin_menus() {
 		return;
 
 	remove_menu_page( 'edit.php' ); // Posts
+	remove_menu_page( 'upload.php' ); // Media
 	remove_menu_page( 'edit-comments.php' ); // Comments
 	remove_menu_page( 'tools.php' ); // Tools
 	remove_menu_page( 'plugins.php' ); // Plugins
