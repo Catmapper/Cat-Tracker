cee98daacf8810b44cd433543ceb288dbfb89f4c
Joey Kudish <info@jkudish.com>
2013-03-13 22:09:46 -0700

    fix js bug when sorting markers
    the sortable_attributes attribute of sightings did not have a length to compute, so use underscore's empty function instead
    fixes bug that was causing the neuter status sorting not to work

--- content/plugins/cat-tracker/resources/cat-tracker.js
+++ content/plugins/cat-tracker/resources/cat-tracker.js
@@ -148,8 +148,9 @@
 					markers.push( __marker );
 
 					// sort the markers into the all_marker_layers_by_attribute array for filtering later on
-					if ( cat_tracker_vars.do_sorting && 'undefined' != typeof( sighting.sortable_attributes ) && sighting.sortable_attributes.length > 0 ) {
+					if ( cat_tracker_vars.do_sorting && 'undefined' != typeof( sighting.sortable_attributes ) && ! _.isEmpty( sighting.sortable_attributes ) ) {
 						$.each( sighting.sortable_attributes, function( attribute_type, attribute_value ){
+							console.log( attribute_type, attribute_value );
 							all_marker_layers_by_attribute[attribute_type][attribute_value].push( __marker );
 						});
 					}
