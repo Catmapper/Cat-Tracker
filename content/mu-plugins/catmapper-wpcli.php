9b45807f7e8c94aaef80a79fe0925eab16cf4b6e
Joey Kudish <joachim.kudish@gmail.com>
2013-07-29 22:02:52 -0700

    wpcli script to fix the sighting type of importer sightings

--- content/mu-plugins/catmapper-wpcli.php
+++ content/mu-plugins/catmapper-wpcli.php
@@ -48,6 +48,28 @@ class Cat_Mapper_Command extends WP_CLI_Command {
 
 	}
 
+	/**
+	 * Update type for each sighting to SPCA intake sighting type
+	 */
+	function updatetype() {
+		$sightings = new WP_Query( array(
+			'post_type' => 'cat_tracker_marker',
+			'posts_per_page' => -1,
+		) );
+		$sighting_type = get_term_by( 'name', 'SPCA Intake Cats', 'cat_tracker_marker_type' );
+		if ( ! $sightings->have_posts() || empty( $sighting_type ) || is_wp_error( $sighting_type ) ) {
+			WP_CLI::error( "Error occurred..." );
+			return;
+		}
+
+		while( $sightings->have_posts() ) {
+			$sightings->the_post();
+			WP_CLI::line( "Updating sighting type for " . get_the_title() . " (ID #" . get_the_ID() . ")" );
+			add_post_meta( get_the_ID(), 'cat_tracker_marker_type', absint( $sighting_type->term_id ), true );
+			wp_set_object_terms( get_the_ID(), absint( $sighting_type->term_id ), 'cat_tracker_marker_type' );
+		}
+	}
+
 }
 
 WP_CLI::add_command( 'catmap', 'Cat_Mapper_Command' );
