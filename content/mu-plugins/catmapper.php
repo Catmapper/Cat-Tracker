f5df82b9621343cf060396f13c58c725e3a6150b
Joey Kudish <joachim.kudish@gmail.com>
2014-01-07 23:28:05 -0800

    prevent fatal errors if cat tracker plugin is not active yet

--- content/mu-plugins/catmapper.php
+++ content/mu-plugins/catmapper.php
@@ -32,6 +32,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+if ( ! class_exists( 'Cat_Tracker' ) )
+	return;
+
 /**
  * remove admin menus that we don't need for the Cat Tracker
  *
