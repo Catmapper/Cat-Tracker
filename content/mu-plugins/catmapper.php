4f6dcf785e8353ab523f7c1d33679505898b96fa
Joey Kudish <joachim.kudish@gmail.com>
2014-01-08 22:51:14 -0800

    adjust the way we detect the presence of the cat tracker

--- content/mu-plugins/catmapper.php
+++ content/mu-plugins/catmapper.php
@@ -32,8 +32,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-if ( ! class_exists( 'Cat_Tracker' ) )
-	return;
+add_action( 'plugins_loaded', function(){
+	if ( ! class_exists( 'Cat_Tracker' ) )
+		exit;
+});
 
 /**
  * remove admin menus that we don't need for the Cat Tracker
